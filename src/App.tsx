937d161ebb53034079cde02d51d95137c728ab52
Travisken <123268134+Travisken@users.noreply.github.com>
2023-10-11 17:35:32 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './css/App.css'
 import './css/navstyle.css'
 import './css/media-screens.css'
 import Body from './Components/body'
+import './assets/7787d49e60dfae3ee0ac23958c6563b8.jpg'
 function App() {
 
 

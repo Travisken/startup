aa76fbaf51543af5453d8239e29c95ddfc8a0920
Travisken <123268134+Travisken@users.noreply.github.com>
2023-10-11 17:09:02 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Nav from './Components/navbar'
+import './css/App.css'
+import './css/navstyle.css'
+import './css/media-screens.css'
+import Body from './Components/body'
+function App() {
+
+
+  return (
+    <>
+
+     <Nav/>
+<Body/>
+    </>
+  )
+}
+
+export default App
